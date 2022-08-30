e319c28bd8efad63e0ec991e2d93fc787fb4ce43
VBychenkova <timka_@bk.ru>
2022-08-30 08:42:40 +0300

    File txt

---
+++ src/main/java/ru/ibs/cources/task3_2/Task32Application.java
@@ -0,0 +1,13 @@
+package ru.ibs.cources.task3_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Task32Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Task32Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ibs/cources/task3_2/Task32ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ibs.cources.task3_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Task32ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
